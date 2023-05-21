ac506c0aa12d49cce83d087406f0ed07d06fbf6a
José Mendible <66981978+mndbl@users.noreply.github.com>
2023-05-20 22:05:46 -0600

    filters

--- src/components/pages/private-routes/data/index.jsx
+++ src/components/pages/private-routes/data/index.jsx
@@ -4,6 +4,10 @@ import { redirect, useLoaderData } from "react-router-dom"
 import { accounts_URL, nf, registers_URL } from "../../../../config/main.config"
 import { dataService } from "../../../../services/data-services"
 import { Table } from "../../../table/table"
+import { useState } from "react"
+import { NumericFormat } from "react-number-format"
+import { useEffect } from "react"
+
 
 const registersColumns = [
     {
@@ -33,7 +37,7 @@ const registersColumns = [
 ]
 
 
-export async function loader({ params, request }) {
+export async function loader({ request }) {
     const userAuth = await localforage.getItem('userAuth')
     const { accessToken } = userAuth
     if (!accessToken) {
@@ -55,22 +59,86 @@ export async function loader({ params, request }) {
 
         register.account_id_deb = debitAccount.name
         register.account_id_cre = creditAccount.name
-        register.amount = nf.format(register.amount)
+        //register.amount = nf.format(register.amount)
     })
     return { registers, accounts, q }
 
 }
 
 export function IndexRegisters() {
-    const { registers } = useLoaderData()
+    const { registers, accounts } = useLoaderData()
+    const [filter, setFilter] = useState('')
+    const [balances, setBalances] = useState({
+        debitsAmount: 0,
+        creditsAmount: 0,
+    })
+    const [balance, setBalance] = useState(0)
+    const [initialBalances, setInitialBalances] = useState({
+        initialDeb: 0,
+        initialCre: 0
+    })
+
     const columns = useMemo(() => registersColumns)
-    const data = useMemo(() => registers)
+
+    const filteredRegisters = filter === '' ?
+        registers :
+        registers.filter((reg) => {
+            return reg.account_id_deb === filter || reg.account_id_cre === filter
+        })
+
+    useEffect(() => {
+        if (filter != '') {
+            if (filteredRegisters[0].account_id_deb === filter) {
+                setInitialBalances(() => ({
+                    initialCre: filteredRegisters[0].account_deb.initial_cre_balance,
+                    initialDeb: filteredRegisters[0].account_deb.initial_cre_balance
+                }))
+            }
+            if (filteredRegisters[0].account_id_cre === filter) {
+                setInitialBalances(() => ({
+                    initialCre: filteredRegisters[0].account_cre.initial_cre_balance,
+                    initialDeb: filteredRegisters[0].account_cre.initial_cre_balance
+                }))
+            }
+
+        }
+        setBalances({
+            creditsAmount: 0,
+            debitsAmount: 0
+        })
+        filteredRegisters.forEach((reg) => {
+
+            return (reg.account_deb.name === filter ?
+                setBalances((prevReg) => ({
+                    ...prevReg,
+                    debitsAmount: initialBalances.initialDeb + prevReg.debitsAmount + reg.amount,
+                })) :
+                reg.account_cre.name === filter ? setBalances((prevReg) => ({
+                    ...prevReg,
+                    creditsAmount: initialBalances.initialCre + prevReg.creditsAmount + reg.amount,
+                })) : 0)
+        })
+        setBalance(balances.debitsAmount - balances.creditsAmount)
+    }, [filter])
+
+    
+    console.log(balance, balances, initialBalances);
+    const data = useMemo(() => filteredRegisters)
+
 
     return (
         <>
             {registers ?
                 <>
-                    <Table columns={columns} data={data} caption='registers' />
+                    <Table columns={columns}
+                        data={data}
+                        caption='registers'
+                        accounts={accounts}
+                        filter={filter}
+                        setFilter={setFilter}
+                        balances={balances}
+                        balance={balance}
+                    />
                 </>
                 :
                 <div>
--- src/components/table/table.jsx
+++ src/components/table/table.jsx
@@ -1,15 +1,15 @@
 import { useTable, useSortBy, usePagination } from "react-table"
-import { OptionsActionsTable } from "./options-actions-table";
 import { ArrowDownIcon, ArrowUpIcon, ChevronRightIcon, ChevronDoubleRightIcon, ChevronDoubleLeftIcon, ChevronLeftIcon } from "@heroicons/react/24/solid";
 import { SearchInput } from "../inputs/search-input";
 import { AddButton } from "../buttons/add-button";
 import { BackButton } from "../buttons/back-button";
-import { useState } from "react";
 import { DataCellOptions } from "./data-cells-options";
+import { useLocation } from "react-router-dom";
+import { useState } from "react";
 
-export function Table({ columns, data, caption, query }) {
-    const [widthScreen, setWidthScreen] = useState()
-
+export function Table({ columns, data, caption, query, filter, setFilter, accounts, balances, balance }) {
+    const location = useLocation()
+    const [filters, setFilters] = useState(false)
     const {
         getTableProps,
         getTableBodyProps,
@@ -34,7 +34,7 @@ export function Table({ columns, data, caption, query }) {
 
     return (
         <div className="flex flex-col w-auto mt-2">
-            
+
             <div className=" overflow-scroll">
                 <div className="flex justify-between py-3 px-2">
                     <div className="relative max-w-xs">
@@ -42,31 +42,81 @@ export function Table({ columns, data, caption, query }) {
                     </div>
 
                     <div className="flex items-center space-x-2">
-                        {/* <div className="relative">
-                            <button className="relative z-0 inline-flex text-sm rounded-md shadow-sm focus:ring-accent-500 focus:border-accent-500 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1">
-                                <span className="relative inline-flex items-center px-3 py-3 space-x-2 text-sm font-medium text-gray-600 bg-white border border-gray-300 rounded-md sm:py-2">
-                                    <div>
-                                        <svg
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            className="w-3 h-3"
-                                            fill="none"
-                                            viewBox="0 0 24 24"
-                                            stroke="currentColor"
-                                            strokeWidth={2}
-                                        >
-                                            <path
-                                                strokeLinecap="round"
-                                                strokeLinejoin="round"
-                                                d="M3 4a1 1 0 011-1h16a1 1 0 011 1v2.586a1 1 0 01-.293.707l-6.414 6.414a1 1 0 00-.293.707V17l-4 4v-6.586a1 1 0 00-.293-.707L3.293 7.293A1 1 0 013 6.586V4z"
-                                            />
-                                        </svg>
+                        {location.pathname === "/dashboard/registers" &&
+
+                            <div className="relative">
+                                {filter &&
+                                    <span className="relative z-0 inline-flex text-sm rounded-md shadow-sm focus:ring-accent-500 focus:border-accent-500 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1
+                                    items-center px-3 py-3 space-x-2 font-medium text-gray-600 bg-white border border-gray-300 sm:py-2 mr-4">
+                                        <p className="">Debits:
+                                            <span className="ml-2 text-blue-800">{balances.debitsAmount}</span>
+                                        </p>
+                                        <p>Credits:
+                                            <span className="ml-2 text-red-700">{balances.creditsAmount}</span>
+                                        </p>
+                                        <p>Balance:
+                                            <span className={`${balance > 0 ? 'text-blue-800' : 'text-red-700'} ml-2`}>{balance}</span>
+                                        </p>
+                                    </span>
+                                }
+                                <button onClick={() => setFilters(!filters)}
+                                    className="relative z-0 inline-flex text-sm rounded-md shadow-sm focus:ring-accent-500 focus:border-accent-500 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1">
+                                    <span className="relative inline-flex items-center px-3 py-3 space-x-2 text-sm font-medium text-gray-600 bg-white border border-gray-300 rounded-md sm:py-2">
+                                        <div>
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                className="w-3 h-3"
+                                                fill="none"
+                                                viewBox="0 0 24 24"
+                                                stroke="currentColor"
+                                                strokeWidth={2}
+                                            >
+                                                <path
+                                                    strokeLinecap="round"
+                                                    strokeLinejoin="round"
+                                                    d="M3 4a1 1 0 011-1h16a1 1 0 011 1v2.586a1 1 0 01-.293.707l-6.414 6.414a1 1 0 00-.293.707V17l-4 4v-6.586a1 1 0 00-.293-.707L3.293 7.293A1 1 0 013 6.586V4z"
+                                                />
+                                            </svg>
+                                        </div>
+                                        <div className="hidden sm:block">
+                                            <span className="capitalize">
+                                                {filter ? filter : 'Filters'}
+                                            </span>
+                                        </div>
+
+                                    </span>
+                                </button>
+                                {filter &&
+                                    <button onClick={() => {
+                                        setFilter('')
+                                        setFilters(false)
+                                    }}
+                                        className="relative z-0 inline-flex text-sm rounded-md shadow-sm focus:ring-accent-500 focus:border-accent-500 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1
+                                     items-center px-3 py-3 space-x-2 font-medium text-gray-600 bg-white border border-gray-300 sm:py-2">
+                                        X</button>
+                                }
+                                {filters &&
+                                    <div className="absolute w-48  h-48 overflow-scroll border-2 border-gray-300 mt-2 bg-white rounded-md">
+                                        {
+                                            accounts.map((account) => {
+                                                return (
+                                                    <button key={account.name + '-filter-' + account.id}
+                                                        onClick={() => {
+                                                            setFilter(account.name);
+                                                            setFilters(false)
+                                                        }}
+                                                        className="hover:bg-gray-500 rounded-md text-xs text-gray-500 hover:text-white w-full text-left p-2">
+                                                        <span className="capitalize"
+                                                        >{filters && account.name}</span>
+                                                    </button>
+
+                                                )
+                                            })
+                                        }
                                     </div>
-                                    <div className="hidden sm:block">
-                                        Filters
-                                    </div>
-                                </span>
-                            </button>
-                        </div> */}
+                                }
+                            </div>
+                        }
                         <AddButton />
                         <BackButton />
                     </div>
